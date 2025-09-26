ea9bb83e1a0371361175ea45491b42f2e0ceee08
fthamid <hamid@unimatel.ma>
2025-09-26 17:38:02 +0200

    version 2 test

--- index.js
+++ index.js
@@ -122,14 +122,21 @@ document.addEventListener('alpine:init', () => {
 
 document.addEventListener('alpine:init', () => {
     Alpine.data(
-        'objentete', (ilastID = 0, iht = 0.00, itva = 0.00, ittc = 0.00, iisremise = false, iremise = 0.00, inet = 0.00) => ({
+        'objentete', (ilastID = 0, iht = 0.00, itva = 0.00, ittc = 0.00, iisremise = false, iremise = 0.00, inet = 0.00, itauxtva= 0.2) => ({
             ht: iht, tva: itva, ttc: ittc, lremise: iremise, net: inet, isremise: iisremise, typepiece: '1',
-            sht: '', stva: '', sttc: '', sremise: '', snet: '', lastID: ilastID,
+            sht: '', stva: '', sttc: '', sremise: '', snet: '',  lastID: ilastID, ltauxtva :itauxtva, stauxtva:'20.00',
             npiece: '', piecevalide: false, pieceremise: false, datepiece: '', client: '', adresse: '', ville: '',
             references: '', rows_nbre: 3, left_show: true, central_show: true,
             sstd: "<td alt=\"supprimer ligne\"><svg  class=\"svg-icon\" style=\"width: 1em; height: 1em;vertical-align: middle;fill: currentColor;overflow: hidden;\" viewBox=\"0 0 1024 1024\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M800 256h-576a30.08 30.08 0 0 0-32 32 30.08 30.08 0 0 0 32 32H256v576a64 64 0 0 0 64 64h384a64 64 0 0 0 64-64V320h32a30.08 30.08 0 0 0 32-32 30.08 30.08 0 0 0-32-32zM448 799.36a33.28 33.28 0 0 1-64 0v-384a33.28 33.28 0 0 1 64 0z m192 0a33.28 33.28 0 0 1-64 0v-384a33.28 33.28 0 0 1 64 0zM800 128H640v-32a32.64 32.64 0 0 0-32-32h-192a32 32 0 0 0-32 32V128H224a30.08 30.08 0 0 0-32 32 30.08 30.08 0 0 0 32 32h576a30.08 30.08 0 0 0 32-32 30.08 30.08 0 0 0-32-32z\" @click=\"deleterow(ID)\" /></svg> </td><td><input size=\"3\" type=\"text\" class=\"nbre\" x-model.lazy=\"ID\" readonly></td><td><input type=\"text\" class=\"article\" x-model.lazy=\"article\" onfocus=\"this.select();\"></td><td><input type=\"text\" name=\"quantite\" class=\"quantite\" x-model.lazy=\"quantite\" onfocus=\"this.select();\" x-mask:dynamic=\"$money($input,\'.\',\' \',2)\"></td><td><input type=\"text\" name=\"prix_u\" class=\"prix\" x-model.lazy=\"prix_u\" onfocus=\"this.select();\" x-mask:dynamic=\"$money($input,\'.\',\' \',2)\"></td><td><input type=\"text\" name=\"mt\" class=\"montant\" x-model=\"smt\" disabled x-init=\"$watch(\'montant\', (value,oldvalue) => updateTotaux(oldvalue,value))\"></td>",
             sstr: "<tr x-ref=\"rowid_ID\" x-id=\"rowid_ID\" x-sort:item x-html=\"" + this.sstd + "\" class=\"sdk\" x-data=\"objline(ID,\'Article________________________________________\'+ID,0,1000)\" x-init=\"updateTotaux(0,prix_u*quantite)\"></tr>",
 
+            get tauxtva() { return this.ltauxtva},
+            set tauxtva(ttva) { 
+                console.log('Taux de Tva:', ttva);
+                this.ltauxtva = ttva/100;
+                this.updateNet();
+                 console.log('Taux de Tva:', ttva, this.ltauxtva);
+            },
             get remise() { return this.lremise; },
             set remise(r) {
                 this.lremise = str2number(r);
@@ -139,8 +146,11 @@ document.addEventListener('alpine:init', () => {
             },
             updateNet() {
                 this.net = this.ttc - this.remise;
-                this.tva = this.net / 6;
-                this.ht = this.net - this.tva;
+                this.ht = this.net/(1+this.tauxtva);
+                this.tva = this.net - this.ht;
+                //this.tva = this.net / 6;
+                //this.ht = this.net - this.tva;
+                this.stauxtva = formatNumber0(this.tauxtva*100);
                 this.sht = formatNumber(this.ht);
                 this.sttc = formatNumber(this.ttc);
                 this.stva = formatNumber(this.tva);
@@ -179,8 +189,22 @@ document.addEventListener('alpine:init', () => {
                 newtr.setAttribute('x-data', "objline(lastID,\'Article_______________________________\'+lastID,1,250)");
                 newtr.setAttribute('class', 'sdk');
                 newtr.setAttribute('x-html', 'sstd');
-                newtr.setAttribute('x-init', 'updateTotaux(0, prix_u*quantite)')
+                newtr.setAttribute('x-init', 'updateTotaux(0, prix_u*quantite)');
+               
                 return newtr;
+            },
+            nouvelleligne() {
+                this.lastID++;
+                this.rows_nbre++;
+                newtr = document.createElement('tr');
+                newtr.setAttribute('id', "rowid_" + this.rows_nbre);
+                newtr.setAttribute('x-data', "objline(lastID,\'Article_______________________________\'+lastID,1,250)");
+                newtr.setAttribute('class', 'sdk');
+                newtr.setAttribute('x-html', 'sstd');
+                newtr.setAttribute('x-init', 'updateTotaux(0, prix_u*quantite)');
+               document.getElementById('corps').appendChild(newtr);
+               document.getElementById('corps').scrollHeight;
+               // return newtr;
             },
 
         }),
