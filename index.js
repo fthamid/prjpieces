c8d4d9464b3e9b94d0b9cf044e5e8eaf8b00960f
fthamid <hamid@unimatel.ma>
2025-09-26 19:40:12 +0200

    third

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ document.addEventListener('alpine:init', () => {
             ht: iht, tva: itva, ttc: ittc, lremise: iremise, net: inet, isremise: iisremise, typepiece: '1',
             sht: '', stva: '', sttc: '', sremise: '', snet: '',  lastID: ilastID, ltauxtva :itauxtva, stauxtva:'20.00',
             npiece: '', piecevalide: false, pieceremise: false, datepiece: '', client: '', adresse: '', ville: '',
-            references: '', rows_nbre: 3, left_show: true, central_show: true,
+            references: '', rows_nbre: 3, left_show: true, central_show: true, 
             sstd: "<td alt=\"supprimer ligne\"><svg  class=\"svg-icon\" style=\"width: 1em; height: 1em;vertical-align: middle;fill: currentColor;overflow: hidden;\" viewBox=\"0 0 1024 1024\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M800 256h-576a30.08 30.08 0 0 0-32 32 30.08 30.08 0 0 0 32 32H256v576a64 64 0 0 0 64 64h384a64 64 0 0 0 64-64V320h32a30.08 30.08 0 0 0 32-32 30.08 30.08 0 0 0-32-32zM448 799.36a33.28 33.28 0 0 1-64 0v-384a33.28 33.28 0 0 1 64 0z m192 0a33.28 33.28 0 0 1-64 0v-384a33.28 33.28 0 0 1 64 0zM800 128H640v-32a32.64 32.64 0 0 0-32-32h-192a32 32 0 0 0-32 32V128H224a30.08 30.08 0 0 0-32 32 30.08 30.08 0 0 0 32 32h576a30.08 30.08 0 0 0 32-32 30.08 30.08 0 0 0-32-32z\" @click=\"deleterow(ID)\" /></svg> </td><td><input size=\"3\" type=\"text\" class=\"nbre\" x-model.lazy=\"ID\" readonly></td><td><input type=\"text\" class=\"article\" x-model.lazy=\"article\" onfocus=\"this.select();\"></td><td><input type=\"text\" name=\"quantite\" class=\"quantite\" x-model.lazy=\"quantite\" onfocus=\"this.select();\" x-mask:dynamic=\"$money($input,\'.\',\' \',2)\"></td><td><input type=\"text\" name=\"prix_u\" class=\"prix\" x-model.lazy=\"prix_u\" onfocus=\"this.select();\" x-mask:dynamic=\"$money($input,\'.\',\' \',2)\"></td><td><input type=\"text\" name=\"mt\" class=\"montant\" x-model=\"smt\" disabled x-init=\"$watch(\'montant\', (value,oldvalue) => updateTotaux(oldvalue,value))\"></td>",
             sstr: "<tr x-ref=\"rowid_ID\" x-id=\"rowid_ID\" x-sort:item x-html=\"" + this.sstd + "\" class=\"sdk\" x-data=\"objline(ID,\'Article________________________________________\'+ID,0,1000)\" x-init=\"updateTotaux(0,prix_u*quantite)\"></tr>",
 
@@ -161,6 +161,10 @@ document.addEventListener('alpine:init', () => {
                 this.ttc = this.ttc - oldmt + newmt;
                 this.updateNet();
             },
+            nbreArticles() {
+                return this.rows_nbre;
+                //document.getElementsByName('quantite').length ;
+            },
             recalcul_piece() {
                 var qt = document.getElementsByName('quantite');
                 var pu = document.getElementsByName('prix_u');
@@ -180,6 +184,7 @@ document.addEventListener('alpine:init', () => {
                 console.log("delete row:", id, 'rowid_' + id)
                 document.getElementById('rowid_' + id).remove();
                 this.recalcul_piece();
+                this.rows_nbre--;
             },
             newrow() {
                 this.lastID++;
@@ -203,7 +208,7 @@ document.addEventListener('alpine:init', () => {
                 newtr.setAttribute('x-html', 'sstd');
                 newtr.setAttribute('x-init', 'updateTotaux(0, prix_u*quantite)');
                document.getElementById('corps').appendChild(newtr);
-               document.getElementById('corps').scrollHeight;
+               document.getElementById('mcorps').scrollTop = document.getElementById('mcorps').scrollHeight-8;
                // return newtr;
             },
 
